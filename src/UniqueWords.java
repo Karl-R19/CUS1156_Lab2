ce5cc31764996f307df9e207ad1701d33cc39165
“Karl <karl.reid21@my.stjohns.edu>
2023-10-02 20:46:02 -0400

    making method that count the number of distinct elements in an ArrayList

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,52 +1,48 @@
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {
-          String currentWord = list.get(i);
-          boolean isUnique = true;
+public class UniqueWords {
+	/**
+	 * counts the number of unique strings in a list
+	 * 
+	 * @param list ArrayList of strings to be examined
+	 * @return number of unique strings in the list
+	 */
+	public static int countUnique(ArrayList<String> list) {
+		int count = 0;
 
-          // Compare the current element with elements that follow
-          for (int j = i + 1; j < list.size(); j++)
-          {
-             if (currentWord.equals(list.get(j)))
-             {
-                isUnique = false;
-                break; 
-             }
-          }
+		for (int i = 0; i < list.size(); i++) {
+			String currentWord = list.get(i);
+			boolean isUnique = true;
 
-          // If the element is unique, increment the count
-          if (isUnique)
-          {
-             count++;
-          }
-       }
-	  return count;
-   }
+			// ----------------------------------------------------
+			// Compare the current word with words that follow
+			for (int j = i + 1; j < list.size(); j++) {
+				if (currentWord.equals(list.get(j))) {
+					isUnique = false;
+					break;
+				}
+			}
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+			// ----------------------------------------------------
+			// If the word is unique, increment the count
+			if (isUnique) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+
+		int unique = countUnique(words);
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
